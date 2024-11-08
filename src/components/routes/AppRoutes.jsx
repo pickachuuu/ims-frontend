ae8818af161ae911b4264d4f08c5e9d9ac1f3f71
Derek <dberb789@gmail.com>
2024-11-08 17:00:09 +0800

    feat: implemented private routing

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import LandingPage from '../../pages/HomePage';
+import PrivateRoutes from './PrivateRoutes';
 
 const AppRoutes =() => {
   const { token, isAuthenticated } = useContext(AuthContext);
@@ -12,6 +13,8 @@ const AppRoutes =() => {
       <Router>
         <Routes>
           <Route path="/" element={!isAuthenticated ? <LandingPage /> : <Navigate to="/Dashboard" />} />
+          <Route element={<PrivateRoutes isAuthenticated={isAuthenticated} />}>
+          </Route>
         </Routes>
       </Router>
   );
