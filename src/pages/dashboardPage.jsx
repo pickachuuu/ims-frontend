dc8f0e9d171c86eec9cdca8d49da2f690915ba90
Derek <dberb789@gmail.com>
2024-11-09 18:29:09 +0800

    feat: add dashboard page component

---
+++ src/pages/dashboardPage.jsx
@@ -0,0 +1,12 @@
+import { React } from 'react'
+
+
+const dashboardPage = () => {
+    return (
+        <>
+            <h1>Dashboard this is a protected route</h1>
+        </>
+    );
+};
+
+export default dashboardPage;
