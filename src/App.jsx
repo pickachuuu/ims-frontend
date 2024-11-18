ce6db7e44cfeb322dbd6c67c0ff657c67050940d
Derek <dberb789@gmail.com>
2024-11-18 18:31:59 +0800

    chore: add Bootstrap CSS and JS imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { AuthProvider }  from '../src/context/AuthContext';
 import AppRoutes from './components/routes/AppRoutes';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
