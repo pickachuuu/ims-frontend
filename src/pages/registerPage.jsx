eca79ba96fa3e2f5ae3242b37a2e0c3dfc304b13
Derek <dberb789@gmail.com>
2024-11-18 15:17:29 +0800

    feat: init register page

---
+++ src/pages/registerPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import RegisterForm from '../components/auth/register';
+import '../App.css';
+
+const RegisterPage = () => {
+    return (
+        <div class="container d-flex justify-content-center align-items-center min-vh-100">
+            <div class="row border rounded-5 p-3 bg-white shadow box-area">
+            {/* left */}
+
+            {/* right */}
+            </div>
+        </div>
+    );
+};
+
+export default RegisterPage;
