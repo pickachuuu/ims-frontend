6d58c564e24cccf98a1336e3370745d99a06f4f0
Derek <dberb789@gmail.com>
2024-11-19 14:42:07 +0800

    feat: setup register page container

--- src/pages/registerPage.jsx
+++ src/pages/registerPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import RegisterForm from '../components/auth/register';
+import SideBanner from '../components/auth/sideBannerReg';
 import '../App.css';
 
 const RegisterPage = () => {
     return (
-        <div class="container d-flex justify-content-center align-items-center min-vh-100">
-            <div class="row border rounded-5 p-3 bg-white shadow box-area">
-            {/* left */}
-
-            {/* right */}
+        <div className="container d-flex justify-content-center align-items-center min-vh-100">
+            <div 
+                className="row border rounded-5 p-3 bg-white shadow mx-auto">
+                    <RegisterForm />
+                    <SideBanner />
             </div>
         </div>
     );
