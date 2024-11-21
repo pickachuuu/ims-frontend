7bdf52af2c8259ced90a2a967fe1a5fe44841b85
Derek <dberb789@gmail.com>
2024-11-21 21:03:29 +0800

    feat: added sidebar

--- src/pages/dashboardPage.jsx
+++ src/pages/dashboardPage.jsx
@@ -1,10 +1,11 @@
 import { React } from 'react'
-
+import Sidebar from '../components/dashboard/sideBar';
 
 const dashboardPage = () => {
     return (
         <>
-            <h1>Dashboard this is a protected route</h1>
+        < Sidebar />
+            {/* <h1>Dashboard this is a protected route</h1> */}
         </>
     );
 };
