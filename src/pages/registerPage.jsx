e615408c1309e0deb115c6c23d8bd916d0f06cd1
Derek <dberb789@gmail.com>
2024-11-21 21:03:54 +0800

    feat: added register form and side banner

--- src/pages/registerPage.jsx
+++ src/pages/registerPage.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import RegisterForm from '../components/auth/register';
 import SideBanner from '../components/auth/sideBannerReg';
+import BG from '../assets/curve.svg';
 import '../App.css';
 
 const RegisterPage = () => {
     return (
-        <div className="container d-flex justify-content-center align-items-center min-vh-100">
-            <div 
-                className="row border rounded-5 p-3 bg-white shadow mx-auto">
-                    <RegisterForm />
-                    <SideBanner />
+        <div className="page-container" style={{ backgroundImage: `url(${BG})` }}>  
+            <div className="container d-flex justify-content-center align-items-center min-vh-100">
+                <div className="row border rounded-5 p-3 bg-white shadow mx-auto">
+                        <SideBanner />
+                        <RegisterForm />
+                </div>
             </div>
         </div>
     );
