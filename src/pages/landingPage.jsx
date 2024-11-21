92b771c8f2779fbc935880ba4bcaaa9189bbaceb
Derek <dberb789@gmail.com>
2024-11-21 21:04:28 +0800

    feat: added login form and side banner

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import LoginForm from '../components/auth/login';
 import SideBanner from '../components/auth/sideBanner';
+import BG from '../assets/curve.svg';
+import logo from '../assets/logo.png';
 
 const LandingPage = () => {
     return (
-        <div className="container d-flex justify-content-center align-items-center min-vh-100">
-            <div 
-                className="row border rounded-5 p-3 bg-white shadow mx-auto">
-                <SideBanner />
-                <LoginForm />
+        <div className="page-container" style={{ backgroundImage: `url(${BG})` }}>
+            <div className="container d-flex justify-content-center align-items-center min-vh-100" >
+                <div 
+                    className="row border rounded-5 p-3 bg-white shadow mx-auto">
+                    <LoginForm />
+                    <SideBanner />
+                </div>
             </div>
         </div>
+
     );
 };
 
