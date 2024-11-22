b73788a1f15c80edb58b8528a630b70d2cd41855
Derek <dberb789@gmail.com>
2024-11-22 16:20:03 +0800

    chore: changed path names

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -4,18 +4,17 @@ import { AuthContext } from '../../context/AuthContext';
 import LandingPage from '../../pages/landingPage';
 import RegisterPage from '../../pages/registerPage';
 import PrivateRoutes from './PrivateRoutes';
-import DashboardPage from '../../pages/dashboardPage';
-
+import HomePage from '../../pages/homePage';
 const AppRoutes =() => {
   const { token, isAuthenticated } = useContext(AuthContext);
 
   return (
       <Router>
         <Routes>
-          <Route path="/" element={!isAuthenticated ? <LandingPage /> : <Navigate to="/Dashboard" />} />
+          <Route path="/" element={!isAuthenticated ? <LandingPage /> : <Navigate to="/Home" />} />
           <Route path='/register' element={< RegisterPage/>}/>
           <Route element={<PrivateRoutes isAuthenticated={isAuthenticated} />}>
-            <Route path="/dashboard" element={< DashboardPage />} />
+            <Route path="/Home" element={< HomePage />} />
           </Route>
         </Routes>
       </Router>
