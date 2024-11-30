0848854cc6749fa91562d0ca1c361d56434e8849
Derek <dberb789@gmail.com>
2024-11-30 12:14:04 +0800

    feat: added rounded container

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -1,6 +1,6 @@
 const CategoriesPage = () => {
     return (
-        <div>
+        <div className="border rounded-5 p-4 bg-white shadow mx-auto">
             <h1>Categories</h1>
         </div>
     );
