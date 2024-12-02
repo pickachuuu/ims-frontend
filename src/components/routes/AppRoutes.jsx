a1f4b9b9aa03fba7689bca98889defe571b800b0
Derek <dberb789@gmail.com>
2024-12-02 22:13:14 +0800

    feat: added multiple routes

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -5,20 +5,34 @@ import LandingPage from '../../pages/landingPage';
 import RegisterPage from '../../pages/registerPage';
 import PrivateRoutes from './PrivateRoutes';
 import HomePage from '../../pages/homePage';
-const AppRoutes =() => {
-  const { token, isAuthenticated } = useContext(AuthContext);
+import Dashboard from '../../pages/dashboard';
+import ProductPage from '../../pages/product';
+import CategoriesPage from '../../pages/categories';
+import LowStockPage from '../../pages/lowstock';
+import SuppliersPage from '../../pages/supplier';
+import ReportsPage from '../../pages/reports';
+
+const AppRoutes = () => {
+  const { isAuthenticated } = useContext(AuthContext);
 
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={!isAuthenticated ? <LandingPage /> : <Navigate to="/Home" />} />
-          <Route path='/register' element={< RegisterPage/>}/>
-          <Route element={<PrivateRoutes isAuthenticated={isAuthenticated} />}>
-            <Route path="/Home" element={< HomePage />} />
+    <Router>
+      <Routes>
+        <Route path="/" element={!isAuthenticated ? <LandingPage /> : <Navigate to="/Home/dashboard" />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route element={<PrivateRoutes isAuthenticated={isAuthenticated} />}>
+          <Route path="/Home" element={<HomePage />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="products" element={<ProductPage />} />
+            <Route path="categories" element={<CategoriesPage />} />
+            <Route path="lowstock" element={<LowStockPage />} />
+            <Route path="suppliers" element={<SuppliersPage />} />
+            <Route path="reports" element={<ReportsPage />} />
           </Route>
-        </Routes>
-      </Router>
+        </Route>
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default AppRoutes;
