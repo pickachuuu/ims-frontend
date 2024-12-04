e1123dc5009cd54787d7d3509c74045649988e57
Derek <dberb789@gmail.com>
2024-12-04 20:36:03 +0800

    feat: add loading state handling in AppRoutes component to improve user experience

--- src/components/routes/AppRoutes.jsx
+++ src/components/routes/AppRoutes.jsx
@@ -13,14 +13,19 @@ import SuppliersPage from '../../pages/supplier';
 import ReportsPage from '../../pages/reports';
 
 const AppRoutes = () => {
-  const { isAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated, loading } = useContext(AuthContext); // Access loading state
+
+  // Show loading message while loading
+  if (loading) {
+    return <h1>Loading...</h1>; // Simple loading message
+  }
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={!isAuthenticated ? <LandingPage /> : <Navigate to="/Home/dashboard" />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route element={<PrivateRoutes  />}>
+        <Route element={<PrivateRoutes />}>
           <Route path="/Home" element={<HomePage />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="products" element={<ProductPage />} />
