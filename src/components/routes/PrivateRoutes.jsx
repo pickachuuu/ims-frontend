ef1eb5188ba37e113f8033b0d4ee8c7dbcd4b938
Derek <dberb789@gmail.com>
2024-12-04 20:35:43 +0800

    refactor: improve code style in PrivateRoutes component by adding spacing for better readability

--- src/components/routes/PrivateRoutes.jsx
+++ src/components/routes/PrivateRoutes.jsx
@@ -2,13 +2,13 @@ import { useContext } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 
-
 const PrivateRoutes = () => {
-  const {isAuthenticated} = useContext(AuthContext)
+  const { isAuthenticated } = useContext(AuthContext);
+
   if (!isAuthenticated) {
     return <Navigate to="/" replace />;
   }
-  return <Outlet/>
+  return <Outlet />;
 };
 
 export default PrivateRoutes;
