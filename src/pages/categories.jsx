53dd3c5a3f8baf05447729c5120033666c39ff62
Derek <dberb789@gmail.com>
2024-12-05 15:03:49 +0800

    fix: adjust height of CategoriesPage container for better layout

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -1,6 +1,6 @@
 const CategoriesPage = () => {
     return (
-        <div className="border rounded-3 p-4 bg-white shadow mx-auto">
+        <div className="border rounded-3 p-4 bg-white shadow mx-auto" style={{height: '95vh'}}>
             <h1>Categories</h1>
         </div>
     );
