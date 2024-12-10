e00dd296a271f7aae898e28817ac3b0dbf27b6c7
Derek <dberb789@gmail.com>
2024-12-10 12:41:57 +0800

    feat: implement search bar and sort by selection

--- src/pages/supplier.jsx
+++ src/pages/supplier.jsx
@@ -1,7 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import {
+    Typography,
+    Button,
+    Box,
+    TextField,
+    Select,
+    MenuItem
+} from '@mui/material';
+
+const handleClickOpen = () => {
+    console.log("test");
+}
+
 const SuppliersPage = () => {
+    const [categories, setCategories] = useState([]);
+    const [products, setProducts] = useState([]); 
+    const [newCategory, setNewCategory] = useState({ name: '', description: '' });
+    const [loading, setLoading] = useState(true);
+    const [open, setOpen] = useState(false);
+    const [editingCategory, setEditingCategory] = useState(null);
+    const [selectedItems, setSelectedItems] = useState([]); 
+    const [searchTerm, setSearchTerm] = useState(''); 
+    const [sortOrder, setSortOrder] = useState(''); 
+
     return (
         <div className="border rounded-3 p-4 bg-white shadow mx-auto" style={{ margin: '0 auto', height: '95vh' }}>
-            <h1>Suppliers</h1>
+            <div className="d-flex justify-content-between align-items-center mb-4">
+                <Typography variant="h4">Supplier</Typography>
+                <Button variant="contained" color="primary" onClick={handleClickOpen}>
+                    + Add Supplier
+                </Button>
+            </div>
+            <hr />
+            <TextField
+                label="Quick search"
+                variant="outlined"
+                size="small"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                style={{ width: '100%', maxWidth: '250px', marginBottom: '20px' }}
+            />
+            <Box className="mb-3" sx={{ display: 'block', marginBottom: '20px' }}>
+                <Select
+                    value={sortOrder}
+                    onChange={(e) => setSortOrder(e.target.value)}
+                    displayEmpty
+                    size='small'
+                    variant="outlined"
+                    sx={{ width: '100%', maxWidth: '130px', marginRight: '10px', marginBottom: '10px' }}
+                >
+                    <MenuItem value="">
+                        <em>Sort by</em>
+                    </MenuItem>
+                    <MenuItem value="asc">A-Z</MenuItem>
+                    <MenuItem value="desc">Z-A</MenuItem>
+                </Select>
+            </Box>
+
         </div>
     );
 };
